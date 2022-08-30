0824bd2440952cc84e88e2e9394461aec94d7471
SaiqG <akkorsakov@gmail.com>
2022-08-30 22:52:06 +0300

    3rd case

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string num1 = Console.ReadLine();
+var iNum1 = int.Parse(num1);
+if (iNum1 % 2 == 0)
+{
+    Console.WriteLine($"{num1} четное");
+}
+else
+{
+    Console.WriteLine($"{num1} не четное");
+}
