3f971c6c8aa0fc353b234cc9cff78b0b7ecbdaa3
SaiqG <akkorsakov@gmail.com>
2022-08-30 22:35:30 +0300

    1st case

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа");
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+var iNum1 = int.Parse(num1);
+var iNum2 = int.Parse(num2);
+if (iNum1 > iNum2)
+{
+   Console.WriteLine("Первое число больше"); 
+}
+else if (iNum1 < iNum2)
+{
+Console.WriteLine("Второе число больше"); 
+}
+else 
+{
+Console.WriteLine("Числа равны"); 
+}
