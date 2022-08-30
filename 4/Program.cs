48ac8aa78ac442edd3d2211269b9323000586638
SaiqG <akkorsakov@gmail.com>
2022-08-30 23:10:19 +0300

    4th case

---
+++ 4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+int iNum = int.Parse(num);
+Console.WriteLine($"От 1 до {iNum} следующие числа четные:");
+for (int i = 1; i <= iNum; i++) 
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+}
