0ead0cc82b70eae27d05849bda85a9ad2084eb3b
SaiqG <akkorsakov@gmail.com>
2022-08-31 13:24:41 +0300

    test :wq

--- 4/Program.cs
+++ 4/Program.cs
@@ -2,7 +2,7 @@
 string num = Console.ReadLine();
 int iNum = int.Parse(num);
 Console.WriteLine($"От 1 до {iNum} следующие числа четные:");
-for (int i = 1; i <= iNum; i++) 
+for (int i = 1; i <= iNum; i++)
 {
     if (i % 2 == 0)
     {
